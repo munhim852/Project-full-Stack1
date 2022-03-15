f645c357b7396a59a481e4002e939ecbe6a769dc
munhim852 <78628991+munhim852@users.noreply.github.com>
2022-03-14 21:45:24 -0400

    Redux config and create Post and form
    1:06:41 finished redux and form section

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json( {limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded( {limit: "30mb", extended: true}));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://admin:admin8964@cluster0.jompk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
