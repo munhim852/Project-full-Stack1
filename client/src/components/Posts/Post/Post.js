c5c0f4bd2211e1a49ab702c7b18cbd52be76adfa
munhim852 <78628991+munhim852@users.noreply.github.com>
2022-04-06 17:21:12 -0400

    Like feature
    like feature

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost} from '../../../actions/posts';
 
 
 
@@ -34,11 +34,12 @@ const Post = ( {post, setCurrentId} ) => {
             <div className={classes.details}>
             <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title} </Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5" color="gutterBottom">{post.message} </Typography>
+                <Typography variant="body2" color="textSecondary" component={"p"}>{post.message} </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => { }}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
                     Like
                     {post.likeCount}
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,7 @@
 export default (posts = [], action) => {
     switch (action.type) {
         case 'UPDATE':
+        case 'LIKE':
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
         case 'FETCH_ALL':
             return action.payload;    
@@ -8,6 +9,7 @@ export default (posts = [], action) => {
             return [ ...posts, action.payload];
         case 'DELETE':
             return posts.filter((post) => post._id !== action.payload);
+        
         default:
             return posts;
     }
