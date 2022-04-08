b8d00fff80dc39b93bee0b79e9915ac30f52281c
munhim852 <78628991+munhim852@users.noreply.github.com>
2022-04-08 16:38:37 -0400

    test
    1

--- server/index1.js
+++ server/index1.js
@@ -6,19 +6,27 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config({ path: 'a.env' });
+dotenv.config();
+
 app.use(bodyParser.json( {limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded( {limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello to Memories API');
 });
+
+// const CONNECTION_URL = "mongodb+srv://admin:admin8964@cluster0.jompk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect('mongodb+srv://admin:admin8964@cluster0.jompk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`Server running on port : ${PORT}`)))
-    .catch((error) => console.log(error.message));
+    .catch((error) => console.log(error.message))
 
+
+
+
+// https://www.mongodb.com/cloud/atlas
